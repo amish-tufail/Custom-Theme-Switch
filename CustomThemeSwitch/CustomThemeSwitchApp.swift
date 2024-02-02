fb5fef2c12a5e0ca2b1f1cdbe2a18d7dbdaa3f82
Amish <amish.tufail2002@gmail.com>
2024-02-02 15:53:51 +0500

    Initial Commit

---
+++ CustomThemeSwitch/CustomThemeSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomThemeSwitchApp.swift
+//  CustomThemeSwitch
+//
+//  Created by Amish Tufail on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomThemeSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
